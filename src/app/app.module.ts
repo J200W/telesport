7c5b5532858535986a75aa74f6f72d8aa46cb1ad
J200W <94389835+J200W@users.noreply.github.com>
2024-02-08 21:16:41 +0100

    create detail page + home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -4,13 +4,15 @@ import { OlympicService } from "src/app/core/services/olympic.service";
 import country from "src/app/core/models/Olympic";
 
 @Component({
-    selector: "app-home",
-    templateUrl: "./home.component.html",
-    styleUrls: ["./home.component.scss"],
+    selector: "app-detail",
+    templateUrl: "./detail.component.html",
+    styleUrls: ["./detail.component.scss"],
 })
 
-export class HomeComponent implements OnInit {
+export class DetailComponent implements OnInit {
+
     public olympics: Observable<country[]> = of([]);
+    // public countrySelected: country = of(null);
 
     constructor(private olympicService: OlympicService) {}
 

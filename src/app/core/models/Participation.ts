bb0a41b1338d73f328e75a1ca6856809e6b75d08
J200W <94389835+J200W@users.noreply.github.com>
2024-02-15 20:49:35 +0100

    Dashboard done

--- src/app/core/models/Participation.ts
+++
@@ -1,9 +0,0 @@
-// TODO: create here a typescript interface for a participation
-
-export default interface participation {
-    id: number;
-    year: number;
-    city: string;
-    medalsCount: number;
-    athleteCount: number;
-}

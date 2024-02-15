807ae6719a72ce797831d34bc55265df58021439
J200W <94389835+J200W@users.noreply.github.com>
2024-02-15 14:50:12 +0100

    Creation of chart component

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
+import country from "../models/Olympic";
 
 @Injectable({
     providedIn: "root",
@@ -9,21 +10,21 @@ import { catchError, tap } from "rxjs/operators";
 export class OlympicService {
     private olympicJSON = "./assets/mock/olympic.json";
 
-    private olympics$ = new BehaviorSubject<any>(undefined);
+    private olympics$ = new BehaviorSubject<Array<country>>([]);
 
     constructor(private http: HttpClient) {}
 
     loadInitialData() {
-        return this.http.get<any>(this.olympicJSON).pipe(
+        return this.http.get<Array<country>>(this.olympicJSON).pipe(
             tap((value) => {
                 this.olympics$.next(value);
-                console.log("value: ", value);
+                console.log("Olympic service value: ", value);
             }),
             catchError((error, caught) => {
                 // TODO: improve error handling
                 console.error(error);
                 // can be useful to end loading state and let the user know something went wrong
-                this.olympics$.next(null);
+                this.olympics$.next([]);
                 return caught;
             })
         );
---
+++ src/app/olympic-chart/olympic-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { Observable, of } from "rxjs";
+import country from "../core/models/Olympic";
+
+@Component({
+    selector: "app-olympic-chart",
+    templateUrl: "./olympic-chart.component.html",
+    styleUrls: ["./olympic-chart.component.scss"],
+})
+export class OlympicChartComponent {
+
+    ngOnInit(): void {}
+
+}
